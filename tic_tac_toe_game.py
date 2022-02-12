1e7d18da6694d4dc54b386c85c103e7beb56301d
Nelson-Newton <Tumwesigenelson94@gmail.com>
2022-02-11 20:32:15 -0800

    Nelson-Newton

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -10,5 +10,30 @@ def display_board():
         print(board[3] + " | " + board[4] + " | " + board[5])
         print(board[6] + " | " + board[7] + " | " + board[8])
 
-        
+def play_turn():
+        x = int(input(f"what is player {turn}`s x position? "))
+        o = int(input(f"what is player {turn}`s o position? "))
+
+        try:
+            if board [x, o] == 0:
+                board [x, o] == turn
+
+            else: 
+              play-turn()
+
+        else: indexError
+        play_turn
+
+turn = 1
+move = 9
+
+while move > 0:
+  print(board)
+  play_turn()
+
+  if win():
+    print(f"congs!!!!.. player {turn} has won")
+    break
+
+                 
         
